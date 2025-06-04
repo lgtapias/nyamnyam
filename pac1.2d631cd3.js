845914f0e7644f2aeca1bbac76a0ff712981325b
Lola <admin@Lolas-Mac-mini.local>
2025-06-04 19:53:48 +0200

    canvis

--- pac1.2d631cd3.js
+++
@@ -1,2 +0,0 @@
-document.addEventListener("DOMContentLoaded",function(){new Splide("#image-slider",{type:"loop",drag:"free",snap:!0,perPage:3,gap:"2rem",breakpoints:{640:{perPage:2,gap:".7rem"},480:{perPage:1,gap:".7rem"}}}).mount()});
-//# sourceMappingURL=pac1.2d631cd3.js.map
